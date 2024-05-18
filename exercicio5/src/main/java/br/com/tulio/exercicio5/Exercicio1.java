9b7bbc4f50557bef2d5f11f604624ec32e6e5de5
tuliodeoliveira <tuliofonseca10@hotmail.com>
2024-05-18 15:01:39 -0300

    commitando exercicio1

---
+++ exercicio5/src/main/java/br/com/tulio/exercicio5/Exercicio1.java
@@ -0,0 +1,23 @@
+package br.com.tulio.exercicio5;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println(somaDosValores(3));
+        
+    }
+    public static int somaDosValores(int n){
+        
+      String n1 = String.valueOf(n);
+        
+      String nn2 = String.valueOf(n+n);
+        
+      String nnn3 = String.valueOf(n+n+n);
+        
+      
+      int soma = Integer.parseInt(n1+nn2+nnn3);
+      
+     return soma; 
+    }
+
+}
